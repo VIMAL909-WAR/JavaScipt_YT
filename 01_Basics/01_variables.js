3e7bce42b56483aa438af21b98991d5ec3021187
VIMAL909-WAR <vp383421@gmail.com>
2024-11-19 05:06:28 +0000

    datatypes and variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+// console.log("Cyber SEcurity");
+const accId = 789456
+var accName = "Sofia"
+let accPassword = "8524658"
+accemail = "123abc2@gmail.com"
+let name;
+
+// accId = 85684
+accName = "Secury"
+accPassword = "2648139"
+accemail = "abf123@gmail.com"
+
+console.table([accId,accName,accPassword,accemail,name])
+
+/* 
+var can use out of block
+
+*/
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+/*
+number -> 2 to the power 53
+bigint
+string -> ""
+boolean -> true/false
+null -> standalone value
+undefined -> 
+synbol -> unique
+
+
+object
+*/
+let n = 10
+let name = "Sofia"
+let isLoggedIn = true
+let sys;
+
+console.log(typeof(n));
+console.log(typeof(name));
+console.log(typeof(isLoggedIn));
+console.log(typeof(sys));
+console.log
+
